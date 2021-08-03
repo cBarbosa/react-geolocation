17aca466ef9afa19350f441ba4910ff8c2cac96f
Charles Barbosa <xbrown@gmail.com>
2021-08-02 23:03:24 -0300

    getting automatic config

--- src/App.js
+++ src/App.js
@@ -1,32 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
     const [lat, setLat] = useState(null);
     const [lng, setLng] = useState(null);
+    const [ip, setIp] = useState(null);
+    const [country, setCountry] = useState(null);
     const [status, setStatus] = useState(null);
+    const [city, setCity] = useState(null);
+    const [state, setState] = useState(null);
 
-    const getLocation = () => {
-        if (!navigator.geolocation) {
-			setStatus('Geolocation is not supported by your browser');
-		} else {
-            setStatus('Locating...');
-            navigator.geolocation.getCurrentPosition((position) => {
-                setStatus(null);
-                setLat(position.coords.latitude);
-                setLng(position.coords.longitude);
-            }, () => {
-                setStatus('Unable to retrieve your location');
-            });
-        }
-    }
+    const API = 'https://geolocation-db.com/json/';
+    const DEFAULT_QUERY = 'redux';
+
+    // const getLocation = () => {
+    //     if (!navigator.geolocation) {
+	// 		setStatus('Geolocation is not supported by your browser');
+	// 	} else {
+    //         setStatus('Localizando...');
+    //         navigator.geolocation.getCurrentPosition((position) => {
+    //             setStatus(null);
+    //             setLat(position.coords.latitude);
+    //             setLng(position.coords.longitude);
+    //         }, () => {
+    //             setStatus('Unable to retrieve your location');
+    //         });
+    //     }
+    // }
+
+    useEffect(()=> {
+
+        (async function() {
+            if (!navigator.geolocation) {
+                setStatus('Geolocation is not supported by your browser');
+            } else {
+                setStatus('Localizando...');
+                navigator.geolocation.getCurrentPosition((position) => {
+                    setStatus(null);
+                    setLat(position.coords.latitude);
+                    setLng(position.coords.longitude);
+                }, () => {
+                    setStatus('Unable to retrieve your location');
+                });
+            }
+        })();
+
+        (async function() {
+            fetch(API + DEFAULT_QUERY)
+                .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+                })
+                .then(data => {
+                    console.debug('result IP', data);
+                    setIp(data.IPv4);
+                    setCountry(data.country_name);
+                    setCity(data.city);
+                    setState(data.state);
+                })
+                .catch(error => console.debug('Get IP', error));
+            }
+        )();
+        
+      }, []);
 
 	return (
 		<div className="App">
-			<button onClick={getLocation}>Get Location</button>
-			<h1>Coordinates</h1>
+			{/* <button onClick={getLocation}>Pegar Localização</button> */}
+			{/* <h1>Coordenadas</h1> */}
             <p>{status}</p>
 			{lat && <p>Latitude: {lat}</p>}
 			{lng && <p>Longitude: {lng}</p>}
+            {ip && <p>IP: {ip}</p>}
+            {country && <p>Pais: {country}</p>}
+            {city && <p>Cidade: {city}</p>}
+            {state && <p>Estado: {state}</p>}
 		</div>
 	);
 }
